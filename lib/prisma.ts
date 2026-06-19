378eea18b26ac7fd4789facfa7ff356373d2dc3c
linshuping205 <linshuping205@gmail.com>
2026-06-19 17:50:26 +0800

    fix global prisma type declaration

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,26 +1,27 @@
-import { PrismaClient } from '@prisma/client' 
- 
-const globalForPrisma = globalThis as unknown as { 
-} 
- 
-function getClient() { 
-  if (!globalForPrisma.prisma) { 
-    globalForPrisma.prisma = new PrismaClient() 
-  } 
-  return globalForPrisma.prisma 
-} 
- 
-export function getPrismaClient() { 
-  return getClient() 
-} 
- 
-export const prisma = new Proxy({} as PrismaClient, { 
-  get(_, prop: string | symbol) { 
-    const client = getClient() 
-    const value = client[prop as keyof PrismaClient] 
-    if (typeof value === 'function') { 
-      return value.bind(client) 
-    } 
-    return value 
-  } 
-}) as PrismaClient 
+import { PrismaClient } from '@prisma/client'
+
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+function getClient() {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  return global.prisma
+}
+
+export function getPrismaClient() {
+  return getClient()
+}
+
+export const prisma = new Proxy({} as PrismaClient, {
+  get(_, prop: string | symbol) {
+    const client = getClient()
+    const value = client[prop as keyof PrismaClient]
+    if (typeof value === 'function') {
+      return value.bind(client)
+    }
+    return value
+  }
+}) as PrismaClient
